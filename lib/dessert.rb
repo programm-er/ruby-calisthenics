136fd48d349a972845323cd1fdbaa14138ff1a94
Juan Martinez <juane.martinez15@est.unanleon.edu.ni>
2018-12-03 06:30:09 -0600

    class dessert is finished

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,48 @@
 class Dessert
+
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
     # your code here
+    @name = name
+    @calories = calories
   end
+  
   def healthy?
     # your code here
+    sano = nil
+    if @calories > 0 && @calories <= 100
+      sano = true
+    else 
+      sano = false
+    end
+
+    sano
   end
   def delicious?
     # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
+
+  #attr_accessor :favor
+
   def initialize(flavor)
     # your code here
+    @name = "#{flavor} jelly bean"
+    @calories = 5
+  end
+  def delicious?
+    tip = @name.split(" ")
+    
+    tip.each do |x|
+      if x == 'licorice'
+        return false
+      else
+        return true
+      end
+    end
   end
 end
