901e9ad832b7aaf1110b7051356b73b7ea54e816
Juan Martinez <juane.martinez15@est.unanleon.edu.ni>
2018-12-03 07:43:30 -0600

    fun_with_strings finished

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,55 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    val = nil
+    let = self.downcase.scan(/\w/)
+
+    if(let == let.reverse)
+      val = true
+    else
+      val = false
+    end
+
+    val
   end
+
   def count_words
     # your code here
+    _hash = Hash.new
+    self.downcase.scan(/\b\w*\b/) do |x|
+      if _hash.has_key?(x)
+        _hash[x] += 1
+      elsif x == " "
+
+      else
+        _hash[x] = 1
+      end
+    end
+
+    _hash
   end
+
   def anagram_groups
-    # your code here
+    # your code 
+    arr = Array.new
+
+    if self.empty?
+      return arr
+    else
+      data = self.split(" ")
+      data.each do |x|
+        t = []
+        data.each do |y|
+          if x.downcase.split(//).sort == y.downcase.split(//).sort
+            t.push(y)            
+          end
+        end
+      end
+
+    arr.push(t)
+    return  arr
+
+    end
   end
 end
 
